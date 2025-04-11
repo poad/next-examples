eadee953feb272d6294f2fba2a9e5d87e7c79660
Kenji Saito <ken-yo@mbr.nifty.com>
2025-04-11 21:22:01 +0900

    Merge repositories

---
+++ with-preact/next.config.js
@@ -0,0 +1,27 @@
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  reactStrictMode: true,
+  cleanDistDir: true,
+  images: {
+    unoptimized: true,
+  },
+
+  webpack: (config, { dev, isServer }) => {
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        "react/jsx-runtime.js": "preact/compat/jsx-runtime",
+        react: "preact/compat",
+        "react-dom/test-utils": "preact/test-utils",
+        "react-dom": "preact/compat",
+      });
+    }
+    return config;
+  },
+};
+
+module.exports = withBundleAnalyzer(nextConfig);

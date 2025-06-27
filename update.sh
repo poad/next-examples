6a254832b0f8d9e110f2cca2f5db14d5a765e87e
Kenji Saito <ken-yo@mbr.nifty.com>
2025-06-27 19:30:37 +0900

    Bumps node modules

--- update.sh
+++ update.sh
@@ -35,6 +35,27 @@ fi
 echo ""
 pwd
 cargo update
+cd "${CURRENT}/with-beta" || exit
+git pull --prune
+result=$?
+if [ $result -ne 0 ]; then
+  cd "${CUR}" || exit
+  exit $result
+fi
+
+result=$?
+if [ $result -ne 0 ]; then
+  cd "${CUR}" || exit
+  exit $result
+fi
+echo ""
+pwd
+npx -y pnpm@latest self-update && pnpm install -r && pnpm up -r && pnpm lint-fix && pnpm build
+result=$?
+if [ $result -ne 0 ]; then
+  cd "${CUR}" || exit
+  exit $result
+fi
 
 cd "${CURRENT}" || exit
 result=$?
